9d11642c53583a1efbf924b35db329b989d16fb7
Saumya Soni <saumyasoni00745@gmail.com>
2025-12-03 22:37:07 +0530

    Tenth Commit

--- Javascript/Day20 DOM/script.js
+++ Javascript/Day20 DOM/script.js
@@ -41,7 +41,7 @@ console.log(span);
 
 /**
  * ! HTML Collection vs NodeList
- */
+ 
 
 const pCollection = document.getElementsByClassName("child"); // [1,2,3]
 const pNodeList = document.querySelectorAll(".child"); // [1,2,3]
@@ -51,3 +51,81 @@ pNodeList[2].remove();
 
 console.log("HTML Collection:", pCollection);
 console.log("Node List:", pNodeList);
+
+*/
+
+/**
+ * ! document.createElement("")
+ * ! appendChild()
+ * ! append()
+ 
+
+const heading = document.createElement("h1");  // <h1></h1>  => { textContent : ""}
+heading.textContent = "Hello Developers!!";
+
+// Parent Element
+const body = document.body;
+body.appendChild(heading);
+
+
+const p1 =  document.createElement("p");  // <p></p>  => {textContent : ""}
+const p2 =  document.createElement("p");  // <p></p>  => {textContent : ""}
+
+p1.textContent = "Child-2";
+p2.textContent = "Child-3";
+
+// const parent = document.getElementsByClassName("parent")[0];  
+// HTML Collection { 0: <div class="parent"></div>}
+
+const parent = document.querySelector(".parent");
+parent.append(p1,p2, "Abra ka dabra");
+*/
+
+
+/**
+ * ! insertAdjacentElement(position, element)
+ * 
+ * position:-  "beforebegin", "afterend", "afterbegin", "afterend"
+
+
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
+
+p1.textContent = "Child-2";
+p2.textContent = "Child-4";
+p3.textContent = "Child-6";
+
+const midP = document.querySelector(".parent .mid");
+
+midP.insertAdjacentElement("beforebegin",p1);
+midP.insertAdjacentElement( "afterend", p2);
+
+const lastP = document.querySelector(".parent :last-child");
+lastP.insertAdjacentElement("beforebegin", p3)
+ */
+
+
+/**
+ * ! insertAdjacentElement(position,element)
+ * position = "afterbegin" , "beforeend"
+ */
+
+const p1 = document.createElement("p");
+const p3 = document.createElement("p");
+
+p1.textContent = "Child-1";
+p3.textContent = "Child-3";
+
+const div = document.querySelector("#parent");
+
+div.insertAdjacentElement("afterbegin",p1);
+div.insertAdjacentElement("beforeend",p3);
+
+
+
+
+
+
+
+
